e49e7f031d4df6dba2b9f423afc1dfe313d501fa
Vijendra Singh <vijendra.singh112@gmail.com>
2024-09-25 22:21:50 +0530

    Initial commit

--- ClientApp/src/app/home/home.component.ts
+++ ClientApp/src/app/home/home.component.ts
@@ -3,15 +3,12 @@ import { AddUserRequest } from '../../appmodels/adduser.model';
 import { UserService } from '../../services/user.service';
 import { Router } from '@angular/router';
 
-UserService
-
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
 })
 export class HomeComponent implements OnInit {
-
-  model: AddUserRequest
+  model: AddUserRequest;
   message!: string;
   result!: string;
 
@@ -19,24 +16,29 @@ export class HomeComponent implements OnInit {
     this.model = {
       name: '',
       address: ''
-    }
+    };
   }
-
 
   ngOnInit() {
-
+    // You can initialize anything if needed here
   }
 
-  AddUser() {
-    this.userService.addUser(this.model).subscribe({
-      next: (response: any) => {
-        this.result = response;
-      },
-      error: (error: any) => {
-        console.log("this was failed" + error);
-      }
-    })
+  AddUser(form: any) {
+    // Ensure form is valid before submitting
+    if (form.valid) {
+      this.userService.addUser(this.model).subscribe({
+        next: (response: any) => {
+          this.result = response;
+          // Optionally, navigate to a different route or reset the form
+          // this.router.navigate(['/some-route']); 
+        },
+        error: (error: any) => {
+          console.error("Error occurred while adding user: ", error);
+          this.message = "Failed to add user. Please try again."; // Optional message for user feedback
+        }
+      });
+    } else {
+      this.message = "Please fill in the required fields.";
+    }
   }
-
 }
-
